319c79ea9806d01edea531db713815f1dec13eec
Unknown <andre.walsh@hotmail.com>
2019-08-25 18:44:49 -0300

    UsuarioDAO impl

---
+++ src/main/java/br/com/sgt/repository/impl/UsuarioDAO.java
@@ -0,0 +1,31 @@
+package br.com.sgt.repository.impl;
+
+import java.util.List;
+
+import br.com.sgt.entities.Usuario;
+import br.com.sgt.repository.api.UsuarioRepository;
+import br.com.sgt.repository.filtro.FiltroUsuario;
+
+public class UsuarioDAO implements UsuarioRepository {
+
+	public Usuario salavar(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Usuario atualizar(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Usuario> buscarPorFiltro(FiltroUsuario filtroUsuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void excluir(Usuario usuario) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
