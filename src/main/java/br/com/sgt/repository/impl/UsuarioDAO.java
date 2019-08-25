831c9c44bab38da99ec449f17011c72be51ebb00
Unknown <andre.walsh@hotmail.com>
2019-08-25 18:46:20 -0300

    Construtor

--- src/main/java/br/com/sgt/repository/impl/UsuarioDAO.java
+++ src/main/java/br/com/sgt/repository/impl/UsuarioDAO.java
@@ -2,14 +2,21 @@ package br.com.sgt.repository.impl;
 
 import java.util.List;
 
+import br.com.sgt.dao.DAO;
 import br.com.sgt.entities.Usuario;
 import br.com.sgt.repository.api.UsuarioRepository;
 import br.com.sgt.repository.filtro.FiltroUsuario;
 
 public class UsuarioDAO implements UsuarioRepository {
+	
+	private DAO<Usuario> dao;
+	
+	public UsuarioDAO() {
+		dao = new DAO<Usuario>(Usuario.class);
+	}
 
 	public Usuario salavar(Usuario usuario) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
