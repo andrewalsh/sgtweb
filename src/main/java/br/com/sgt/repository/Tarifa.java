b970780e745cd5c84b6272d37b18b66bc27291d2
Unknown <andre.walsh@hotmail.com>
2019-08-25 16:05:37 -0300

    Repositorio ValorAutorizado

--- src/main/java/br/com/sgt/repository/Tarifa.java
+++ src/main/java/br/com/sgt/repository/Tarifa.java
@@ -2,9 +2,7 @@ package br.com.sgt.repository;
 
 import java.util.List;
 
-import br.com.sgt.entities.TipoTarifa;
 import br.com.sgt.repository.filtro.FiltroTarifa;
-import br.com.sgt.repository.filtro.FiltroTipoTarifa;
 
 public interface Tarifa {
 
---
+++ src/main/java/br/com/sgt/repository/ValorAutorizadoRepository.java
@@ -0,0 +1,17 @@
+package br.com.sgt.repository;
+
+import java.util.List;
+
+import br.com.sgt.entities.ValorAutorizado;
+import br.com.sgt.repository.filtro.FiltroValorAutorizado;
+
+public interface ValorAutorizadoRepository {
+	
+	ValorAutorizado salavar(ValorAutorizado valorAutorizado);
+	
+	ValorAutorizado atualizar(ValorAutorizado valorAutorizado);
+	
+	List<ValorAutorizado> buscarPorFiltro(FiltroValorAutorizado filtroValorAutorizado);
+	
+	void excluir(ValorAutorizado valorAutorizado);
+}
